59650e86b4460d5478e199d192c67f7d064c3069
tsaiwentage <gzzxcaiwentian@163.com>
2020-03-02 11:11:18 +0800

    Update data path

--- config.py
+++ config.py
@@ -51,7 +51,9 @@ STEP_PER_EPOCH = 500
 EVAL_EPOCH = 10
 
 # data path
-BASEDIR = "/data/dataset/BRATS2018/" #'/data/dataset/BRATS2018/'
+#basedir="/data/dataset/BRATS2018/{mode}/{HGG/LGG}/patient_id/{flair/t1/t1ce/t2/seg}"
+#BASEDIR = "/data/dataset/BRATS2018/" #'/data/dataset/BRATS2018/'
+BASEDIR = "/data/3DUnet-Tensorflow-Brats18"
 TRAIN_DATASET = ['training']
 VAL_DATASET = 'val'   # val or val17 
 TEST_DATASET = 'val'
